71b291785d95e48ee1b94a448bb3b78cc4a6fd49
YagoCardoso23 <144952712+YagoCardoso23@users.noreply.github.com>
2023-10-13 10:29:35 -0300

    Create Personagem.java

---
+++ Personagem.java
@@ -0,0 +1,11 @@
+public interface Personagem {
+    String getNome();
+    int calcularIniciativa();
+    int calcularAtaque();
+    int calcularDefesa();
+    int calcularDano(int ataque, int defesa);
+    boolean estaVivo();
+    void atacar(Personagem alvo);
+    void levarDano(int dano);
+   
+}
